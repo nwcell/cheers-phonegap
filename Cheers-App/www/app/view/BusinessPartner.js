c70670336230a1d2849243ce6331e430860d4459
Rey <padilla.reyj@gmail.com>
2012-12-14 07:34:19 +0700

    updates

---
+++ Cheers-App/www/app/view/BusinessPartner.js
@@ -0,0 +1,24 @@
+         
+Ext.define("Cheers.view.BusinessPartner", {
+    extend: "Ext.Panel",  
+     
+     modal: true,
+    config: {
+              
+             scrollable:true,
+               
+               width:300,
+               height:200,
+               fullscreen:false,
+             items:[
+                    {
+                        html: 'test?' 			
+                             
+                             
+                    } 
+             ]
+
+          
+     }
+});
+
